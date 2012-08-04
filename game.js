5beb51bf17dff6e3022ab4982fc99a57926556d3
Christopher Hall <Biomech.ArkahnX@gmail.com>
2012-08-03 21:05:05 -0700

    rebuilt app.js

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
 //Game.js
 
-const CANVAS_WIDTH =	800;
-const CANVAS_HEIGHT = 600;
+const CANVAS_WIDTH 	=	800;
+const CANVAS_HEIGHT =	800;
+var inGame = new Boolean();
 
 //Create canvas element
 var canvasElement = $("<canvas width='" + CANVAS_WIDTH +
@@ -10,7 +10,7 @@ var canvasElement = $("<canvas width='" + CANVAS_WIDTH +
 var canvas = canvasElement.get(0).getContext("2d");
 canvasElement.appendTo('div#game');
 
-// var fps = 30;
+var fps = 30;
 // setInterval(function()
 // {
 //   update();
@@ -19,28 +19,3 @@ canvasElement.appendTo('div#game');
 
 //Global variables
 inGame = false;
-=======
-
-	//Game.js
-
-	const CANVAS_WIDTH 	=	800;
-	const CANVAS_HEIGHT =	800;
-	var inGame = new Boolean();
-
-	//Create canvas element
-	var canvasElement = $("<canvas width='" + CANVAS_WIDTH +
-	                      "' height='" + CANVAS_HEIGHT + "'></canvas>");
-	var canvas = canvasElement.get(0).getContext("2d");
-	canvasElement.appendTo('div#game');
-
-	var fps = 30;
-	setInterval(function()
-	{
-	  update();
-	  draw();
-	}, 1000/fps);
-
-	//Global variables
-	inGame = false;
-
->>>>>>> upstream/master
