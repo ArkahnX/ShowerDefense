1f39403b3ce574aca324d7d10c105dd75f9a5298
Lucas Penney <lucaspenney@gmail.com>
2012-08-03 19:28:12 -0700

    Removed unneccesary files

--- js/fadein.js
+++
@@ -1,7 +0,0 @@
-// JavaScript Document
-// Fade in the entire page
-// Set body style="display:none;" for this to work. 
-
-$(document).ready(function() {
-	$('body').fadeIn('slow');
-});
--- js/hmenu.js
+++
@@ -1,5 +0,0 @@
-// JavaScript Document
-//hmenu.js
-//Horizontal (dropdown) menu with jQuery
-
-$('.hmenu li').hover
